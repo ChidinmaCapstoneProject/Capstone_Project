48d53e7eac3c0c386306e676bbae6ba65a6c8e1d
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-07-17 20:05:39 -0700

    User Login and Authentication based on chidinmachinedu collection in mongodb

---
+++ context/AuthProvider.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+import useLocalStorage from "../../Hooks/useLocalStorage";
+
+const AuthContext = createContext({});
+
+export const AuthProvider =({children}) =>{
+    const [auth, setAuth] = useState({});
+    const [userName, setUserName] = useLocalStorage("userName", '');
+    const [chooseClass, setChooseClass] = useLocalStorage("chooseClass", '');
+    const [traineeEmail, setTraineeEmail] = useLocalStorage("traineeEmail", '');
+
+    return(
+        <AuthContext.Provider value={{auth, setAuth, userName, setUserName, chooseClass, setChooseClass, traineeEmail, setTraineeEmail}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
