c618508ebd9e2bb3c94cc957d4291fde4e5ff930
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-07-19 13:53:52 -0700

    Only 1 Sidebar component determines what sidebar will be displayed: Trainer or Trainee

--- Home/Home.jsx
+++ Home/Home.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { useContext } from "react";
-import Sidebar from "../Trainer/Sidebar/Sidebar";
-import SidebarC from "../Trainee/SidebarC/SidebarC";
-import AuthContext from "../context/AuthProvider";
+import Sidebar from "../Sidebar/Sidebar";
 import "./Home.css";
 export default function Home() {
-  const { chooseClass } = useContext(AuthContext);
   return (
     <div className="home-div">
-      {chooseClass == "Trainer" ? <Sidebar /> : <SidebarC />}
-      hi friends
+      <Sidebar />
       <br />
       <a href="https://www.fhittingroom.com/in-studio/">
         <img src="https://aktivsolutions.com/wordpress/wp-content/uploads/2020/03/Fhit-Room-Shop-Aktiv.png"></img>
