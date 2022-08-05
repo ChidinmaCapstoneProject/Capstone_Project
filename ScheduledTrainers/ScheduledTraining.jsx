d6c7c763b29f6fbc14cd7fe721343f6055b5d8cf
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-05 09:42:32 -0700

    The content at the side of the calender that displays selected day trainings

---
+++ ScheduledTrainers/ScheduledTraining.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import useLocalStorage from "../../../../Hooks/useLocalStorage";
+import { getAllTrainings } from "../../../../Utils/DataManagement";
+import { SOCKET_URL } from "../../../../Utils/URLConstants";
+import { useEffect } from "react";
+import { parseISO, isSameDay } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
+import { NO_TRAINERS } from "../../../../Utils/StringLiterals";
+const socket = io(SOCKET_URL);
+
+export default function ScheduledTraining({ selectedDay }) {
+  const [initialTrainings, setInitialTrainings] = useLocalStorage(
+    "initialTrainings",
+    []
+  );
+  const [trainings, setTrainings] = useLocalStorage("trainings", []);
+  const [selectedDayTrainings, setSelectedDayTrainings] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getTrainings = async () => {
+      setInitialTrainings(await getAllTrainings());
+    };
+    socket.on("newTraining", (Training) => {
+      getTrainings();
+    });
+    socket.on("deletedTraining", (id) => {
+      const updatedTraining = initialTrainings.filter((training) => {
+        return training._id !== id;
+      });
+      getTrainings();
+    });
+    getTrainings();
+  }, []);
+
+  useEffect(() => {
+    const allTrainings = () => {
+      const filtered = initialTrainings.filter((each) => {
+        return isSameDay(parseISO(each?.day), selectedDay);
+      });
+      setTrainings(filtered);
+      setSelectedDayTrainings(
+        filtered.filter(
+          (ele, ind) => ind == filtered.findIndex((elem) => elem.Id === ele.Id)
+        )
+      );
+    };
+    allTrainings();
+  }, [selectedDay, initialTrainings]);
+
+  const handleClick = (trainerName) => {
+    navigate("/TrainerInfo", {
+      state: { trainerName: trainerName, trainings: trainings },
+    });
+  };
+  return (
+    <div>
+      <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+        {selectedDayTrainings?.length > 0 ? (
+          selectedDayTrainings.map((each, id) => (
+            <li
+              key={id}
+              className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100"
+              onClick={() => handleClick(each.fullname)}
+            >
+              <div className="flex-auto">
+                <p className="text-gray-900"> {each.fullname}</p>
+              </div>
+              <p className="mt-0.2 text-gray-400">Click For More Info</p>
+            </li>
+          ))
+        ) : (
+          <p>{NO_TRAINERS}</p>
+        )}
+      </ol>
+    </div>
+  );
+}
---
+++ Utils/URLConstants.js
@@ -0,0 +1,7 @@
+export const REVIEWS_URL = "/review";
+export const LOGIN_URL = "/auth";
+export const TRAININGS_URL = "/training";
+export const BOOKINGS_URL = "/booking";
+export const REGISTER_URL = "/register";
+export const LOGOUT_URL = "/logout";
+export const SOCKET_URL = "http://localhost:8900";
