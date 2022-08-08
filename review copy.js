c567d37e6dc0259de71df06aa613d25117637e8f
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-08 10:42:36 -0700

    Trainer can update trainings

--- review copy.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const reviewController = require('../controllers/reviewsController')
-
-router.post('/', reviewController.handleNewReview);
-router.get('/', reviewController.getAllReviews);
-
-
-module.exports = router;
