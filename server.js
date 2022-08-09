511754c2769e7d059a33707f17911abf0c0f9e83
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-09 10:24:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -40,14 +40,6 @@ app.use(express.json());
 //middleware for cookies
 app.use(cookieParser());
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("disconnect", () => {
-    console.log("socket.io: User disconnected: ", socket.id);
-  });
-});
-
 //routes
 app.use("/register", register);
 app.use("/auth", auth);
@@ -65,7 +57,6 @@ app.use(verifyJWT);
 mongoose.connection.once("open", () => {
   console.log("Connected to MongoDB");
   app.listen(PORT, () => {
-    console.log(`🚀 Server listening at http://localhost:${PORT}`);
   });
   const trainingCollection = mongoose.connection.collection("trainings");
   const changeStream = trainingCollection.watch();
