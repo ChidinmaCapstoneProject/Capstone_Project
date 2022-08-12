bde9cc90eb22fcd370dbf83866d74cd1d8c87ac2
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-12 15:12:02 -0700

    Capstone Project

--- context/AuthProvider.js
+++ context/AuthProvider.js
@@ -3,17 +3,31 @@ import useLocalStorage from "../../Hooks/useLocalStorage";
 
 const AuthContext = createContext({});
 
-export const AuthProvider =({children}) =>{
-    const [auth, setAuth] = useState({});
-    const [userName, setUserName] = useLocalStorage("userName", '');
-    const [chooseClass, setChooseClass] = useLocalStorage("chooseClass", '');
-    const [traineeEmail, setTraineeEmail] = useLocalStorage("traineeEmail", '');
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  const [userName, setUserName] = useLocalStorage("userName", "");
+  const [userId, setUserId] = useLocalStorage("userId", "");
+  const [chooseClass, setChooseClass] = useLocalStorage("chooseClass", "");
+  const [traineeEmail, setTraineeEmail] = useLocalStorage("traineeEmail", "");
 
-    return(
-        <AuthContext.Provider value={{auth, setAuth, userName, setUserName, chooseClass, setChooseClass, traineeEmail, setTraineeEmail}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+        userName,
+        setUserName,
+        chooseClass,
+        setChooseClass,
+        traineeEmail,
+        setTraineeEmail,
+        userId,
+        setUserId,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
