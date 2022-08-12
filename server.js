b1e0e7692cf45e2cac0cf277353688ccb9f547c0
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-12 14:48:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ app.use("/placeDetails", placeDetails);
 app.use(verifyJWT);
 
 mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
   app.listen(PORT, () => {
   });
   const trainingCollection = mongoose.connection.collection("trainings");
