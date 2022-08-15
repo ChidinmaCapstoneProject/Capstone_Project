59fb15f9f166281fd649f5b03631a3334a6978ae
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-15 16:54:44 -0700

    Update training.js

--- training.js
+++ training.js
@@ -4,5 +4,7 @@ const trainingController = require("../controllers/trainingController");
 
 router.post("/", trainingController.handleNewTraining);
 router.get("/", trainingController.getAllTrainings);
+router.put("/:trainingId", trainingController.updateTraining);
+router.delete("/:trainingId", trainingController.deleteTraining);
 
 module.exports = router;
