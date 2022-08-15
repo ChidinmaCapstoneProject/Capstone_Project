56250c08fbe17968b73c84257fc7f3481a0fe0b8
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-15 16:55:38 -0700

    Update trainingController.js

--- trainingController.js
+++ trainingController.js
@@ -42,7 +42,6 @@ const handleNewTraining = async (req, res) => {
 
     res.status(201).json({ success: `New Training for' ${userName} created` });
   } catch (err) {
-    console.log(err);
     res.status(500).json({ message: err.message });
   }
 };
@@ -52,8 +51,44 @@ const getAllTrainings = async (req, res) => {
     return res.status(204).json({ message: "No Trainings Found" });
   res.json(trainings);
 };
-
+const updateTraining = async (req, res) => {
+  if (!req.params.trainingId) {
+    return res.status(400).json({ message: "ID parameter required" });
+  }
+  const training = await Training.findOne({ _id: req.params.trainingId }).exec();
+  if (!training) {
+    return res
+      .status(204)
+      .json({ message: `No Training ID matches ${req.params.trainingId} ` });
+  }
+  if (req.body?.email) training.email = req.body.email;
+  if (req.body?.trainingType) training.trainingType = req.body.trainingType;
+  if (req.body?.description) training.description = req.body.description;
+  if (req.body?.price) training.price = req.body.price;
+  if (req.body?.slots) training.slots = req.body.slots;
+  if (req.body?.startTime) training.startTime = req.body.startTime;
+  if (req.body?.endTime) training.email = req.body.email;
+  if (req.body?.day) training.day = req.body.day;
+  const result = await training.save();
+  res.json(result);
+};
+const deleteTraining = async (req, res) => {
+  try {
+    const training = await Training.findOne({ _id: req.params.trainingId }).exec();
+    if (!training) {
+      return res
+        .status(404)
+        .json({ message: `No Training ID matches ${req.params.trainingId} ` });
+    }
+    const result = await Training.deleteOne({ _id: req.params.trainingId });
+    res.json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 module.exports = {
   handleNewTraining,
   getAllTrainings,
+  updateTraining,
+  deleteTraining,
 };
