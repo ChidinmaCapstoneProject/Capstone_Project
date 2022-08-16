1cbef94707e17345e9531bb61571ccc62e3758d1
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-16 11:38:27 -0700

    Update bookings.js

--- Backend/bookings.js
+++ Backend/bookings.js
@@ -4,6 +4,7 @@ const bookingController = require('../controllers/bookingsController')
 
 router.post('/', bookingController.handleNewBooking);
 router.get('/', bookingController.getAllBookings);
+router.put('/:trainingId', bookingController.updateBookings);
 router.delete('/:bookingId', bookingController.deleteBooking)
 
 module.exports = router;
