3e44e0089eb5e8f62f2bcded5fbcf8cbbedb92c1
chidinmac <87202479+chidinmac@users.noreply.github.com>
2022-08-16 16:02:50 -0700

    Update Home.jsx

--- capstone-ui/src/Components/Home/Home.jsx
+++ capstone-ui/src/Components/Home/Home.jsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="home-div">
       <Sidebar />
-
       <Logout />
+      <img src="https://cdn4.vectorstock.com/i/1000x1000/61/98/fitness-background-in-flat-style-vector-3876198.jpg" />
     </div>
   );
 }
